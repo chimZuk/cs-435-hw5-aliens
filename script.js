4fbe7f3d62daa390b5067a4b32ebdd1cc2f524a1
Dzmitry Kuzmitch <kuzya19989@outlook.com>
2019-11-04 00:43:38 -0500

    Added alphabet creation and alphabet determination

--- script.js
+++ script.js
@@ -24,39 +24,83 @@ eef`;
 
 function processData(input) {
     var data = input.split("\n");
-    var vertices_count = Number(data[0]) + 1;
-    var edges_count = Number(data[vertices_count].split(" ")[1]);
-    var edges_index = vertices_count + 1;
-    var vertices = data.slice(1, vertices_count);
-    var edges = data.slice(edges_index, edges_index + edges_count).map(x => x.split(" ").map(y => Number(y)));
-
-    var trip = new Euro_ishTrip(vertices, edges);
+    var words = data.slice(1);
+    var alphabet = new Aliens(words);
+    alphabet.print()
 }
 
 
-class Euro_ishTrip {
-    constructor(vertices, edges) {
+class Aliens {
+    constructor(words) {
+        this.words = words;
+        this.graph = [];
+        this.letters = [];
+
         this.explored = [];
         this.pre_sorted = [];
-        this.directions = [];
+        this.alphabet = [];
+        this.alphabet_length = 0;
+        this.alphabet_string = "";
 
+        this.alphabet_creation(copy_array_1d(this.words));
+        this.topological_sort();
+    }
 
-        this.cities = copy_array_1d(vertices);
-        this.edges = copy_array_2d(edges);
+    alphabet_creation(words) {
+        var last_letter = words[0][0];
+        var words_queue = [];
 
-        this.graph = this.cities.map(function(element, i) {
-            element = [];
+        words.forEach(function(word) {
+            if (word[0] == last_letter) {
+                words_queue.push(word.slice(1));
+            } else {
+                this.add_letter(last_letter);
+                this.add_letter(word[0]);
 
-            this.edges.forEach(function(edge) {
-                if (edge[0] == i) {
-                    element.push(edge[1]);
+                this.add_edge(last_letter, word[0]);
+
+                if (this.alphabet_creation_empty_check(words_queue)) {
+                    this.alphabet_creation(copy_array_1d(words_queue));
                 }
-            });
 
-            return element;
+                last_letter = word[0];
+                words_queue = [word.slice(1)];
+            }
         }.bind(this));
 
-        this.get_routes();
+        if (this.alphabet_creation_empty_check(words_queue)) {
+            this.alphabet_creation(copy_array_1d(words_queue));
+        }
+        this.add_letter(last_letter);
+
+        last_letter = "";
+        words_queue = [];
+    }
+
+    alphabet_creation_empty_check(words_queue) {
+        for (var i = 0; i < words_queue.length; i++) {
+            if (words_queue[i] == "") {
+                words_queue.splice(i, 1);
+                i--;
+            }
+        }
+        return words_queue.length != 0;
+    }
+
+    add_letter(letter) {
+        if (this.letters.indexOf(letter) == -1) {
+            this.letters.push(letter);
+            this.graph.push([]);
+        }
+    }
+
+    add_edge(a, b) {
+        var index_of_a = this.letters.indexOf(a);
+        var index_of_b = this.letters.indexOf(b);
+
+        if (this.graph[index_of_a].indexOf(index_of_b) == -1) {
+            this.graph[index_of_a].push(index_of_b);
+        }
     }
 
     topological_sort_helper(index, explored, pre_sorted) {
@@ -72,43 +116,33 @@ class Euro_ishTrip {
     }
 
     topological_sort() {
-        this.graph.forEach(function(vertice, index) {
+        this.graph.forEach(function(edge, index) {
             if (this.explored.indexOf(index) == -1) {
                 this.topological_sort_helper(index, this.explored, this.pre_sorted);
             }
         }.bind(this));
 
-        var temp_direction = [];
+        var temp_aplhabet = [];
 
         while (this.pre_sorted.length != 0) {
-            temp_direction.push(this.cities[this.pre_sorted.splice(0, 1)[0]]);
+            temp_aplhabet.unshift(this.letters[this.pre_sorted.splice(0, 1)[0]]);
         }
-        this.directions.push(temp_direction);
+        this.alphabet = copy_array_1d(temp_aplhabet);
+        this.alphabet_length = this.alphabet.length;
+
+        this.alphabet.forEach(function(element, i) {
+            if (i != this.alphabet_length - 1) {
+                this.alphabet_string += element + " ";
+            } else {
+                this.alphabet_string += element;
+            }
+        }.bind(this));
     }
 
-    output_routes() {
-        this.directions.forEach(function(route) {
-            var route_result = "";
-
-            route.forEach(function(city, index) {
-                if (index != 0) {
-                    route_result = city + ", " + route_result;
-                } else {
-                    route_result = city;
-                }
-            });
-
-            console.log(route_result);
-        });
-    }
-
-    get_routes() {
-        this.topological_sort()
-        this.output_routes();
+    print() {
+        console.log(this.alphabet_string);
     }
 }
-
-
 
 function copy_array_1d(arr1) {
     return arr1.slice();
